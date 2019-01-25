6eab8b7fa0f5c1bbc4d4417f202e8e4df71ae302
curtisagain <curtis.aloia@gmail.com>
2019-01-24 20:26:07 -0800

    Do HW

---
+++ Assignments/H1P1.py
@@ -0,0 +1,12 @@
+
+first_name = input('Please enter your first name:')
+# saves input as variable
+last_name = input('Please enter your last name:')
+age = int(input('Please enter your age:'))
+# using int converts string to integer to do maths
+confidence = float(input('What is your confidence in programmers between 0-100%?'))
+# floats don't have to be whole numbers
+dog_age = (age * 7)
+print('Hello', first_name, last_name, ', nice to meet you! You might be', age, 'in human years, but in dog years you are', dog_age,'.')
+if confidence < 50: print("I agree, programmers can't be trusted!")
+else: print("Writing good code takes hard work!")    

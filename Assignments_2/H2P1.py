0efcb2fdd7f8e968a587f2bed5a183c6d6c912db
curtisagain <curtis.aloia@gmail.com>
2019-01-29 20:21:19 -0800

    hw

--- Assignments_2/H2P1.py
+++ Assignments_2/H2P1.py
@@ -1,8 +1,8 @@
 age = int(input("Enter your age. "))
 age = age + 10
-print("In ten years, you'll be " + str(age) + " .")
+print("In ten years, you'll be " + str(age) + ".")
 
 degreesF = float(input("Enter the current temperature in degrees Fahreheit. "))
 degreesC = (degreesF - 32) * 5 / 9
 degreesC = round(degreesC, 3)
-print("The current temperature in Celsius is" + str(degreesC) + " degrees.")
+print("The current temperature in Celsius is " + str(degreesC) + " degrees.")

b336d8caf89564b1b0f716231ee5dbf343637697
curtisagain <curtis.aloia@gmail.com>
2019-01-29 18:29:25 -0800

    hw

--- Assignments/H1P1.py
+++ Assignments/H1P1.py
@@ -8,5 +8,7 @@ confidence = float(input('What is your confidence in programmers between 0-100%?
 # floats don't have to be whole numbers
 dog_age = (age * 7)
 print('Hello', first_name, last_name, ', nice to meet you! You might be', age, 'in human years, but in dog years you are', dog_age,'.')
-if confidence < 50: print("I agree, programmers can't be trusted!")
-else: print("Writing good code takes hard work!")    
+if (confidence/100) < .5: 
+    print("I agree, programmers can't be trusted!")
+else: 
+    print("Writing good code takes hard work!")    

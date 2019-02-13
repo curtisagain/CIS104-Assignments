3364461d3a83a425e45876ab87cc33bbd4e14987
curtisagain <curtis.aloia@gmail.com>
2019-02-12 20:42:13 -0800

    hw

---
+++ Assignments_3/main.py
@@ -0,0 +1,9 @@
+import calculator
+
+num1 = input("Enter first operand: ")
+op = input("Enter operator: ")
+num2 = input("Enter second operand: ")
+
+if op == "+":
+    calculator.add
+    print(calculator.inputNumber)

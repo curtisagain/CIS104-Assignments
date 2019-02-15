e499190a566e5a8168e205c2a28a98c7532ef1c5
curtisagain <curtis.aloia@gmail.com>
2019-02-14 20:58:05 -0800

    closer

--- Assignments_3/calculator.py
+++ Assignments_3/calculator.py
@@ -1,39 +1,46 @@
+answer = 0
+mem = 0
 # This function adds two numbers 
 def add(num1, num2):
-   return num1 + num2
+   answer = num1 + num2
+   return print(answer)
 
 # This function subtracts two numbers 
 def subtract(num1, num2):
-   return num1 - num2
-
+   answer = num1 - num2
+   return print(answer)
 # This function multiplies two numbers
 def multiply(num1, num2):
-   return num1 * num2
+   answer = num1 * num2
+   return print(answer)
 
 # This function divides two numbers
 def divide(num1, num2):
-   return num1 / num2 
+   answer = num1 / num2
+   return print(answer)
         # https://www.programiz.com/pnum2thon-programming/examples/calculator
 
 # This function finds the power
 def pow(num1, num2):
-    return num1 ** num2
+    answer = num1 ** num2
+   return print(answer)
 
 # This function inverts answer
 def invert(answer):
-    return 0.0 - answer
+    answer = 0.0 - answer
+    return print(answer)
 
 # This function saves answer
 def save(answer, mem):
     answer = mem
-    return 0
+    return print(mem)
 
 # This function recalls memory
 def recall(mem, answer):
     mem = answer
-    return 0
+    return print(answer)
 
 # This function clears memory
 def memClear(mem):
     mem = 0
-    return 0
+    return print("Memory Cleared.")
--- Assignments_3/main.py
+++ Assignments_3/main.py
@@ -1,14 +1,26 @@
 import calculator
 
-print(">")
-entry = ""
-op = ("+", "-", "*", "/", "^")
-calcFunc = ("s", "S", "c", "C", "r", "R", "m", "M")
-num1 = 0
-num2 = 0
-answer = 0
+num1 = float(input("Enter first number: "))
+print("Select operation.")
+print("+ Add")
+print("- Subtract")
+print("* Multiply")
+print("/ Divide")
+print("^ Power")
+print("i Invert")
+print("s Save")
+print("r Recall")
+print("c Clear Memory")
+entry = input("> ")
+num2 = float(input("Enter second number: "))
+
+
+
 # all variables in main
 while entry != 'X' and entry !='x':
-   
+  if entry == "+":
+    calculator.add(num1,num2)
 
-print("Goodbye.")
+  elif entry == "-":
+    calculator.subtract(num1,num2)
+

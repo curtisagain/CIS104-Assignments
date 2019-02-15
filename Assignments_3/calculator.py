8034d4b7d6646eec1984e942ce2caca9edabe62e
curtisagain <curtis.aloia@gmail.com>
2019-02-14 20:14:09 -0800

    found link

--- Assignments_3/calculator.py
+++ Assignments_3/calculator.py
@@ -1,6 +1,15 @@
-answer = 0
 def calculate(num1, num2, op):
+
     if op == "+":
-        answer = num1 + num2
+        return (num1 + num2)
     if op == "-":
-        answer = num1 - num2
+        return (num1 - num2)
+    if op == "/":
+        return (num1 / num2)
+    if op == "*":
+        return (num1 * num2)
+    if op == "^":
+        return (num1 ** num2)
+    if op == "+":
+        return (num1 + num2)    
+        # https://www.programiz.com/python-programming/examples/calculator

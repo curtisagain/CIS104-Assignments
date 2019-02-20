7df82a364f58d8430b33dd109b313e41471c9690
curtisagain <curtis.aloia@gmail.com>
2019-02-19 17:53:03 -0800

    right before the buzzer, added invalid entry.

--- Assignments_3/calculator.py
+++ Assignments_3/calculator.py
@@ -1,46 +1,49 @@
-answer = 0
-mem = 0
 # This function adds two numbers 
 def add(num1, num2):
-   answer = num1 + num2
-   return print(answer)
+   return num1 + num2
 
 # This function subtracts two numbers 
 def subtract(num1, num2):
-   answer = num1 - num2
-   return print(answer)
+   return num1 - num2
+
 # This function multiplies two numbers
 def multiply(num1, num2):
-   answer = num1 * num2
-   return print(answer)
+   return num1 * num2
 
 # This function divides two numbers
 def divide(num1, num2):
-   answer = num1 / num2
-   return print(answer)
+   return num1 / num2
         # https://www.programiz.com/pnum2thon-programming/examples/calculator
 
 # This function finds the power
 def pow(num1, num2):
-    answer = num1 ** num2
-    return print(answer)
+    return num1 ** num2
 
 # This function inverts answer
-def invert(answer):
-    answer = 0.0 - answer
-    return print(answer)
+def invert(num1):
+    return 0.0 - num1
 
 # This function saves answer
-def save(answer, mem):
-    answer = mem
-    return print(mem)
+def save(answer):
+    global mem
+    mem = answer
+    return 0
 
 # This function recalls memory
-def recall(mem, answer):
-    mem = answer
-    return print(answer)
+def recall(num1):
+    global mem
+    num1 = mem
+    return (num1)
 
 # This function clears memory
-def memClear(mem):
+def memClear():
+    global mem
     mem = 0
-    return print("Memory Cleared.")
+    return 0
+
+    # This function clears the whole calculator
+def clear(num1):
+    global mem
+    mem = 0
+    num1 = 0
+    return 0
--- Assignments_3/main.py
+++ Assignments_3/main.py
@@ -10,48 +10,85 @@ print("^ Power")
 print("i Invert")
 print("s Save")
 print("r Recall")
-print("c Clear Memory")
+print("m Clear Memory")
+print("c Clear")
 print("x Exit")
 entry = input("> ")
-num2 = float(input("Enter second number: "))
-
-
-
-# all variables in main
-while entry != 'X' and entry !='x':
-  if entry == "+":
-    calculator.add(num1,num2)
-    from calculator import answer
-  elif entry == "-":
-    calculator.subtract(num1,num2)
-    from calculator import answer
-  elif entry == "*":
-    calculator.multiply(num1,num2)  
-    from calculator import answer
-  elif entry == "/":
-    calculator.divide(num1,num2)
-    from calculator import answer
-  elif entry == "^":
-    calculator.pow(num1,num2)
-    from calculator import answer
-  elif entry == "i" or "I":
-    calculator.invert(num1,num2) 
-    from calculator import answer
-  elif entry == "s" or "S":
-    calculator.save(num1,num2) 
-    from calculator import answer
-  elif entry == "r" or "R":
-    calculator.recall(num1,num2) 
-    from calculator import answer
-  elif entry == "c" or "C":
-    calculator.memClear(num1,num2) 
-    from calculator import answer
-  else:
-    print("Invalid input.")          
+# Like a do while loop in python
+while True :
+  if (entry == "+" or entry == "-" or entry == "*" or entry == "/" or entry == "^"):
+    
+    num2 = float(input("Enter second number: "))
   
-  num1 = answer
-  entry = input(print("> ")) 
-  num2 = float(input("Enter second number: "))   
-  if entry != "x" or "X":
-      continue
+    if entry == "+":
+      answer = calculator.add (num1,num2)
+      num1 = answer
+      print (answer)
+      entry = input("> ")
+  
+    elif entry == "-":
+      answer = calculator.subtract (num1,num2)
+      num1 = answer
+      print (answer)
+      entry = input("> ")
+  
+    elif entry == "*":
+      answer = calculator.multiply (num1,num2)  
+      num1 = answer
+      print (answer)
+      entry = input("> ")
 
+    elif entry == "/":
+      answer = calculator.divide (num1,num2)
+      num1 = answer
+      print (answer)
+      entry = input("> ")
+
+    elif entry == "^":
+      answer = calculator.pow(num1,num2)
+      num1 = answer
+      print (answer)
+      entry = input("> ")
+
+    else:
+      print("Invalid input.")
+  # Elif in case I want to add more in the future
+  elif (entry != "+" or entry != "-" or entry != "*" or entry != "/" or entry != "^"):
+    if (entry == "i" or entry == "I"):
+      answer = calculator.invert(num1) 
+      num1 = answer
+      print (answer)
+      entry = input("> ")
+  
+    elif (entry == "m" or entry == "M"):
+      calculator.memClear() 
+      print ("Memory cleared.")
+      print (answer)
+      entry = input("> ")
+  
+    elif (entry == "c" or entry == "C"):
+      calculator.clear(num1) 
+      num1 = float(input("Cleared. Enter first number: "))
+      entry = input("> ")
+ 
+    elif (entry == "s" or entry == "S"):
+      calculator.save(answer) 
+      num1 = float(input("Saved. Enter first number: "))
+      entry = input("> ")
+
+    elif (entry == "r" or entry == "R"):
+      answer = calculator.recall(num1) 
+      num1 = answer
+      print ("Recalled.")
+      print (answer)
+      entry = input("> ")
+    # x will break loop later.
+    elif (entry == "x" or entry == "X"):
+      print ("Goodbye!")
+    
+    else:
+      print ("Invalid entry.")
+      num1 = float(input("Enter first number: "))
+
+    if entry != "x" or "X":
+      continue         
